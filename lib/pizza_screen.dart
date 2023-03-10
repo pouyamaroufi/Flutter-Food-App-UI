f462bad6bea79d54c66c47f5dd0d6bf73a9e3831
pouyamaroufi <pouyaapps@gmail.com>
2023-03-10 16:25:04 +0330

    add animation

--- lib/pizza_screen.dart
+++ lib/pizza_screen.dart
@@ -6,6 +6,7 @@ import 'package:foodapp/salad_screen.dart';
 import 'package:foodapp/triangle_painter.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class PizzaScreen extends StatelessWidget {
   final controller = Get.put(Controller());
@@ -53,176 +54,194 @@ class PizzaScreen extends StatelessWidget {
                 ? const Center(
                     child: CircularProgressIndicator(),
                   )
-                : Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        height: 150,
-                      ),
-                      Text(
-                        controller
-                            .foodList[controller.contentIndex.value].name!,
-                        style: GoogleFonts.getFont('Lobster', fontSize: 25),
-                      ),
-                      CarouselSlider.builder(
-                        itemCount: controller.foodList.length,
-                        itemBuilder: (BuildContext context, int i,
-                                int pageViewIndex) =>
-                            Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 5),
-                                child: Image.asset(
-                                    'assets/images/${controller.foodList[i].image!}')),
-                        options: CarouselOptions(
-                            height: 390,
-                            aspectRatio: 1,
-                            viewportFraction: 0.7,
-                            initialPage: 0,
-                            enlargeStrategy: CenterPageEnlargeStrategy.scale,
-                            enableInfiniteScroll: true,
-                            reverse: false,
-                            autoPlay: false,
-                            enlargeCenterPage: true,
-                            onPageChanged: (i, change) {
-                              controller.contentIndex.value = i;
-                            }),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                : AnimationLimiter(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: AnimationConfiguration.toStaggeredList(
+                        duration: const Duration(milliseconds: 375),
+                        childAnimationBuilder: (widget) => SlideAnimation(
+                          verticalOffset: 150.0,
+                          child: FadeInAnimation(
+                            child: widget,
+                          ),
+                        ),
                         children: [
-                          const Icon(
-                            Icons.heart_broken_rounded,
-                            color: Colors.grey,
+                          const SizedBox(
+                            height: 150,
                           ),
                           Text(
-                            '${controller.foodList[controller.contentIndex.value].price!} \$',
-                            style: GoogleFonts.getFont('Barlow',
-                                fontSize: 25, fontWeight: FontWeight.w600),
+                            controller
+                                .foodList[controller.contentIndex.value].name!,
+                            style: GoogleFonts.getFont('Lobster', fontSize: 25),
                           ),
-                          const Icon(
-                            Icons.favorite_rounded,
-                            color: Colors.grey,
+                          CarouselSlider.builder(
+                            itemCount: controller.foodList.length,
+                            itemBuilder: (BuildContext context, int i,
+                                    int pageViewIndex) =>
+                                Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 5),
+                                    child: Image.asset(
+                                        'assets/images/${controller.foodList[i].image!}')),
+                            options: CarouselOptions(
+                                height: 390,
+                                aspectRatio: 1,
+                                viewportFraction: 0.7,
+                                initialPage: 0,
+                                enlargeStrategy:
+                                    CenterPageEnlargeStrategy.scale,
+                                enableInfiniteScroll: true,
+                                reverse: false,
+                                autoPlay: false,
+                                enlargeCenterPage: true,
+                                onPageChanged: (i, change) {
+                                  controller.contentIndex.value = i;
+                                }),
                           ),
-                        ],
-                      ),
-                      const SizedBox(height: 35),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          IconButton(
-                            iconSize: 35,
-                            onPressed: () {
-                              if (controller
-                                      .foodList[controller.contentIndex.value]
-                                      .item !=
-                                  0) {
-                                controller
-                                    .foodList[controller.contentIndex.value]
-                                    .item = controller
-                                        .foodList[controller.contentIndex.value]
-                                        .item! -
-                                    1;
-                                controller.removeOrder(controller
-                                    .foodList[controller.contentIndex.value]
-                                    .id!);
-                              }
-                            },
-                            icon: const Icon(
-                              Icons.remove,
-                            ),
-                          ),
-                          CircleAvatar(
-                            radius: 40,
-                            backgroundColor: Colors.amber[300]!,
-                            child: Center(
-                              child: Text(
-                                controller
-                                    .foodList[controller.contentIndex.value]
-                                    .item
-                                    .toString(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Icon(
+                                Icons.heart_broken_rounded,
+                                color: Colors.grey,
+                              ),
+                              Text(
+                                '${controller.foodList[controller.contentIndex.value].price!} \$',
                                 style: GoogleFonts.getFont('Barlow',
-                                    fontSize: 28,
-                                    fontWeight: FontWeight.w600,
-                                    color: Colors.black),
+                                    fontSize: 25, fontWeight: FontWeight.w600),
                               ),
-                            ),
+                              const Icon(
+                                Icons.favorite_rounded,
+                                color: Colors.grey,
+                              ),
+                            ],
                           ),
-                          IconButton(
-                            iconSize: 35,
-                            onPressed: () {
-                              controller.foodList[controller.contentIndex.value]
-                                  .item = controller
+                          const SizedBox(height: 35),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                iconSize: 35,
+                                onPressed: () {
+                                  if (controller
+                                          .foodList[
+                                              controller.contentIndex.value]
+                                          .item !=
+                                      0) {
+                                    controller
+                                        .foodList[controller.contentIndex.value]
+                                        .item = controller
+                                            .foodList[
+                                                controller.contentIndex.value]
+                                            .item! -
+                                        1;
+                                    controller.removeOrder(controller
+                                        .foodList[controller.contentIndex.value]
+                                        .id!);
+                                  }
+                                },
+                                icon: const Icon(
+                                  Icons.remove,
+                                ),
+                              ),
+                              CircleAvatar(
+                                radius: 40,
+                                backgroundColor: Colors.amber[300]!,
+                                child: Center(
+                                  child: Text(
+                                    controller
+                                        .foodList[controller.contentIndex.value]
+                                        .item
+                                        .toString(),
+                                    style: GoogleFonts.getFont('Barlow',
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w600,
+                                        color: Colors.black),
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                iconSize: 35,
+                                onPressed: () {
+                                  controller
                                       .foodList[controller.contentIndex.value]
-                                      .item! +
-                                  1;
-                              controller.addOrder(controller
-                                  .foodList[controller.contentIndex.value].id!);
-                            },
-                            icon: const Icon(
-                              Icons.add,
-                            ),
+                                      .item = controller
+                                          .foodList[
+                                              controller.contentIndex.value]
+                                          .item! +
+                                      1;
+                                  controller.addOrder(controller
+                                      .foodList[controller.contentIndex.value]
+                                      .id!);
+                                },
+                                icon: const Icon(
+                                  Icons.add,
+                                ),
+                              ),
+                            ],
                           ),
+                          Container(
+                            padding: const EdgeInsets.all(10),
+                            margin: const EdgeInsets.all(20),
+                            decoration: BoxDecoration(
+                                // border: Border.all(
+                                //     color:
+                                //         const Color.fromARGB(255, 193, 193, 193)),
+                                borderRadius: BorderRadius.circular(20)),
+                            child: controller.orderList.isEmpty
+                                ? null
+                                : Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      Expanded(
+                                        flex: 5,
+                                        child: AvatarStack(
+                                          height: 50,
+                                          avatars: [
+                                            for (var n = 0;
+                                                n < controller.orderList.length;
+                                                n++)
+                                              AssetImage(
+                                                  'assets/images/${controller.orderList[n].image!}'),
+                                          ],
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 7,
+                                        child: Text(
+                                          'X ${controller.orderList.length}',
+                                          textAlign: TextAlign.end,
+                                          style: GoogleFonts.getFont('Barlow',
+                                              fontSize: 28,
+                                              fontWeight: FontWeight.w600,
+                                              color: Colors.black),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                          ),
+                          if (controller.orderList.isNotEmpty)
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.amber,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 130, vertical: 10),
+                                ),
+                                onPressed: () {
+                                  controller.contentIndex.value = 0;
+                                  Get.to(() => SaladScreen());
+                                },
+                                child: const Text(
+                                  'Continue 🍕',
+                                  style: TextStyle(
+                                      color: Colors.black, fontSize: 18),
+                                ))
                         ],
                       ),
-                      Container(
-                        padding: const EdgeInsets.all(10),
-                        margin: const EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                            // border: Border.all(
-                            //     color:
-                            //         const Color.fromARGB(255, 193, 193, 193)),
-                            borderRadius: BorderRadius.circular(20)),
-                        child: controller.orderList.isEmpty
-                            ? null
-                            : Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Expanded(
-                                    flex: 5,
-                                    child: AvatarStack(
-                                      height: 50,
-                                      avatars: [
-                                        for (var n = 0;
-                                            n < controller.orderList.length;
-                                            n++)
-                                          AssetImage(
-                                              'assets/images/${controller.orderList[n].image!}'),
-                                      ],
-                                    ),
-                                  ),
-                                  Expanded(
-                                    flex: 7,
-                                    child: Text(
-                                      'X ${controller.orderList.length}',
-                                      textAlign: TextAlign.end,
-                                      style: GoogleFonts.getFont('Barlow',
-                                          fontSize: 28,
-                                          fontWeight: FontWeight.w600,
-                                          color: Colors.black),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                      ),
-                      if (controller.orderList.isNotEmpty)
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Colors.amber,
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 130, vertical: 10),
-                            ),
-                            onPressed: () {
-                              controller.contentIndex.value = 0;
-                              Get.to(() => SaladScreen());
-                            },
-                            child: const Text(
-                              'Continue 🍕',
-                              style:
-                                  TextStyle(color: Colors.black, fontSize: 18),
-                            ))
-                    ],
+                    ),
                   )
           ],
         ),
