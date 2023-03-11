0f15418ddd24805ba2cf9dfab807be7c9ef01f67
pouyamaroufi <pouyaapps@gmail.com>
2023-03-11 19:26:03 +0330

    incoice btns

--- lib/invoice_screen.dart
+++ lib/invoice_screen.dart
@@ -19,67 +19,93 @@ class InvoiceScreen extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Obx(
-          () => Column(
-            children: [
-              AnimationLimiter(
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const ClampingScrollPhysics(),
-                  itemCount: controller.invoiceList.length,
-                  itemBuilder: (BuildContext context, int i) =>
-                      AnimationConfiguration.staggeredList(
-                    position: i,
-                    duration: const Duration(milliseconds: 375),
-                    child: SlideAnimation(
-                      duration: const Duration(milliseconds: 575),
-                      horizontalOffset: 150.0,
-                      child: FadeInAnimation(
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          leading: Image.asset(
-                              'assets/images/${controller.invoiceList[i].image!}'),
-                          title: Text(
-                            controller.invoiceList[i].name!,
-                            style: GoogleFonts.getFont('Barlow',
-                                fontWeight: FontWeight.w600),
-                          ),
-                          subtitle: Text(
-                            '${controller.invoiceList[i].price!} \$',
-                          ),
-                          trailing: Text(
-                            'X ${controller.invoiceList[i].item.toString()}',
+      body: Stack(children: [
+        SingleChildScrollView(
+          child: Obx(
+            () => Column(
+              children: [
+                AnimationLimiter(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: const ClampingScrollPhysics(),
+                    itemCount: controller.invoiceList.length,
+                    itemBuilder: (BuildContext context, int i) =>
+                        AnimationConfiguration.staggeredList(
+                      position: i,
+                      duration: const Duration(milliseconds: 375),
+                      child: SlideAnimation(
+                        duration: const Duration(milliseconds: 575),
+                        horizontalOffset: 150.0,
+                        child: FadeInAnimation(
+                          child: ListTile(
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 10),
+                            leading: Image.asset(
+                                'assets/images/${controller.invoiceList[i].image!}'),
+                            title: Text(
+                              controller.invoiceList[i].name!,
+                              style: GoogleFonts.getFont('Barlow',
+                                  fontWeight: FontWeight.w600),
+                            ),
+                            subtitle: Text(
+                              '${controller.invoiceList[i].price!} \$',
+                            ),
+                            trailing: Text(
+                              'X ${controller.invoiceList[i].item.toString()}',
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const Divider(),
-              ListTile(
-                title: Text(
-                  'Total Price:',
-                  style: GoogleFonts.getFont('Barlow',
-                      fontWeight: FontWeight.w600, fontSize: 20),
+                const Divider(),
+                ListTile(
+                  title: Text(
+                    'Total Price:',
+                    style: GoogleFonts.getFont('Barlow',
+                        fontWeight: FontWeight.w600, fontSize: 20),
+                  ),
+                  subtitle: Countup(
+                    begin: 0,
+                    end: controller.totalPrice.value,
+                    duration: const Duration(seconds: 1),
+                    separator: ',',
+                    suffix: '  \$',
+                    curve: Curves.easeIn,
+                    style: GoogleFonts.getFont('Barlow',
+                        fontWeight: FontWeight.w600, fontSize: 72),
+                  ),
                 ),
-                subtitle: Countup(
-                  begin: 0,
-                  end: controller.totalPrice.value,
-                  duration: const Duration(seconds: 1),
-                  separator: ',',
-                  suffix: '  \$',
-                  curve: Curves.easeIn,
-                  style: GoogleFonts.getFont('Barlow',
-                      fontWeight: FontWeight.w600, fontSize: 46),
-                ),
-              )
-            ],
+                const SizedBox(height: 100),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 130, vertical: 10),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      'Pay with PayPal',
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                    )),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 130, vertical: 10),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      '     Apple Pay     ',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ))
+              ],
+            ),
           ),
         ),
-      ),
+      ]),
     );
   }
 }
